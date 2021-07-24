27bd6e76b274503eced9f3cf466310f082608c7b
Rahul-8588 <87888364+Rahul-8588@users.noreply.github.com>
2021-07-24 11:04:30 +0530

    Add files via upload

---
+++ FileData/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ThirdPartyTools;
+
+namespace FileData
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                FileDetails _objFile = new FileDetails();
+                if (args[0].Contains("v"))
+                {
+                    Console.WriteLine(_objFile.Version(args[0]));
+                }
+                else if (args[0].Contains("s"))
+                {
+                    Console.WriteLine(_objFile.Size(args[0]));
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
